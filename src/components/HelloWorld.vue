ddd928ed063b07d75508e1a6de0b7df845c307ac
赵美玲 <zhaomeiling@zhaomeilingdeMacBook-Pro.local>
2020-01-16 16:06:08 +0800

    My Vue Demo

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="hello">
+    <h1>{{ txt }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      txt: 'Welcome to my first vueProject'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  div{
+    background: red;
+  }
+
+</style>
